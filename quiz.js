783882e242d4b12462cf91bc8ef38f94bd38299d
Divya Punjabi <116953971+hard-to-smart@users.noreply.github.com>
2024-09-18 18:10:45 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,80 +1,134 @@
+// import questions from './questions.js'
+
+// const start = document.getElementById('start-section')
+// let currentQuestion=0;
+// let correctAnswerCount=0;
+
+// start.addEventListener('click', ()=>startQuiz())
+
+// // start the function
+// function startQuiz(){
+//     start.style.display = "none";
+//     changeQuestion()
+//     setInterval(changeQuestion, 5000);
+// }
+
+
+// function addTimer() {
+//     const timerSpace = document.getElementById("timer");
+//     let timeLeft = 5;
+//     const timer = setInterval(() => {
+        
+//     timerSpace.innerHTML = timeLeft--;
+//         if (timeLeft < 1 ) {
+//             clearInterval(timer);
+//         }
+//     }, 1000);
+//     currentQuestion++;
+
+   
+// }
+
+  
+  
+
+
+// function changeQuestion() {
+//     const questionBox = document.querySelector("h2");
+//     const optionsBox = document.querySelectorAll("li");
+//     if(currentQuestion >= questions.length ){
+//         endQuiz()
+//         return;
+//     }
+//     questionBox.textContent = questions[currentQuestion].question;
+//     // console.log(questions[currentQuestion].question);
+//     // console.log(currentQuestion)
+//     // for( let i = 0; i< 4 ; i++ ) {
+//     //     optionsBox[i].textContent = questions[currentQuestion].options[i];
+//     //     optionsBox[i].classList.add("color-li");
+//     //     optionsBox[i].addEventListener("click", function(){
+//     //         console.log(questions[currentQuestion].options[i])
+//     //         if (questions[currentQuestion].options[i] == questions[currentQuestion].answer) {
+//     //             correctAnswerCount++;
+//     //         }
+//     //     });
+
+//     // }   
+//     optionsBox.forEach((option, index)=>{
+//         option.textContent = questions[currentQuestion].options[index];
+//         option.classList.add("color-li");
+        
+//         option.addEventListener("click", function(){
+            
+//             console.log("Option clicked:" , questions[currentQuestion].question)
+//             // if (questions[currentQuestion].options[index] == questions[currentQuestion].answer) {
+//             //     correctAnswerCount++;
+//             // }
+
+//         });
+
+//     });
+//         addTimer()
+
+
+
+// }
+// function endQuiz(){
+//     alert(`quiz ended` + correctAnswerCount)
+
+// }
+
 import questions from './questions.js'
 
-console.log(questions)
+const start = document.getElementById('start-section');
+const timerSpace = document.getElementById("timer");
+const questionSpace = document.querySelector('h2');
+const optionsSpace = document.querySelectorAll('li');
 
-const start = document.getElementById('start-section')
+let currectQuestion = 0;
+let correctAnswerCount = 0;
 
-start.addEventListener('click', ()=>startQuiz())
-function startQuiz(){
-    setInterval(changeQuestion, 10000);
+start.addEventListener('click', ()=>{
     start.style.display = "none";
-    addTimer();
+    timerSpace.style.display="block";
+    addTimer()
+})
 
-}
-
-
-function addTimer() {
-    let timeLeft = 10;
-    const timer = setInterval(() => {
-        if (timeLeft <= 0) {
-            clearInterval(timer);
+function addTimer(){
+    let time = 5;
+    const timer = setInterval(()=>{
+        timerSpace.innerHTML = time--;
+        if(currectQuestion.length ==questions.length){
+            return resultSection()
         }
-        timeLeft--;
-    }, 1000);
-    const timerSpace = document.createElement("span");
-    timerSpace.innerHTML = timeLeft;
-    document.body.appendChild(timerSpace);
-}
+        if(time < 0){
+            clearInterval(timer)
+            currectQuestion++;
+        }
+        else{
+            // changeQuestion()
+        }
 
-
-
-let currentQuestion=0;
-let correctAnswerCount=0;
-function changeQuestion() {
-    const questionBox = document.querySelector("h2");
-    const optionsBox = document.querySelectorAll("li");
-    if(currentQuestion >= questions.length ){
-        endQuiz()
-        return;
-    }
-    questionBox.textContent = questions[currentQuestion].question;
-    console.log(questions[currentQuestion].question);
-    console.log(currentQuestion)
-    // for( let i = 0; i< 4 ; i++ ) {
-    //     optionsBox[i].textContent = questions[currentQuestion].options[i];
-    //     optionsBox[i].classList.add("color-li");
-    //     optionsBox[i].addEventListener("click", function(){
-    //         console.log(questions[currentQuestion].options[i])
-    //         if (questions[currentQuestion].options[i] == questions[currentQuestion].answer) {
-    //             correctAnswerCount++;
-    //         }
-    //     });
-    // }   
-    optionsBox.forEach((option, index)=>{
-        option.textContent = questions[currentQuestion].options[index];
-        option.classList.add("color-li");
-
-        option.replaceWith(option.cloneNode(true));
-
-        option.addEventListener("click", function(){
-            console.log("Option clicked:" , questions[currentQuestion].options[index])
-            if (questions[currentQuestion].options[index] == questions[currentQuestion].answer) {
-                correctAnswerCount++;
-            }
-
-        });
-        currentQuestion = (currentQuestion + 1);
-
-    });
-
-}
-function endQuiz(){
-    alert(`quiz ended` + correctAnswerCount)
+    }, 1000)
 
 }
 
+function changeQuestion(){
+    questionSpace.innerHTML = questions[currectQuestion].question;
+    optionsSpace.forEach(( option, index) => {
+        option.textContent = questions[currectQuestion].options[index];
+        option.classList.add("color-li")
+    })
+    optionsSpace.forEach((option)=>{
+        option.addEventListener('click', ()=>{
+            option.classList.add('li-selected')
+            if(option.textContent == questions[currectQuestion].answer)
+            console.log(questions[currectQuestion].answer)
+        })
+    })
 
+}
 
-
-
-
+function resultSection(){
+    const result = document.getElementById("")
+}
