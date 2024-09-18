5c0ab25745b65f2d6dee01f20fac9b858f7e1b7e
hard-to-smart <2403punjabidivya@gmail.com>
2024-09-19 00:55:49 +0530

    finally done

--- quiz.js
+++ quiz.js
@@ -1,134 +1,82 @@
-// import questions from './questions.js'
-
-// const start = document.getElementById('start-section')
-// let currentQuestion=0;
-// let correctAnswerCount=0;
-
-// start.addEventListener('click', ()=>startQuiz())
-
-// // start the function
-// function startQuiz(){
-//     start.style.display = "none";
-//     changeQuestion()
-//     setInterval(changeQuestion, 5000);
-// }
-
-
-// function addTimer() {
-//     const timerSpace = document.getElementById("timer");
-//     let timeLeft = 5;
-//     const timer = setInterval(() => {
-        
-//     timerSpace.innerHTML = timeLeft--;
-//         if (timeLeft < 1 ) {
-//             clearInterval(timer);
-//         }
-//     }, 1000);
-//     currentQuestion++;
-
-   
-// }
-
-  
-  
-
-
-// function changeQuestion() {
-//     const questionBox = document.querySelector("h2");
-//     const optionsBox = document.querySelectorAll("li");
-//     if(currentQuestion >= questions.length ){
-//         endQuiz()
-//         return;
-//     }
-//     questionBox.textContent = questions[currentQuestion].question;
-//     // console.log(questions[currentQuestion].question);
-//     // console.log(currentQuestion)
-//     // for( let i = 0; i< 4 ; i++ ) {
-//     //     optionsBox[i].textContent = questions[currentQuestion].options[i];
-//     //     optionsBox[i].classList.add("color-li");
-//     //     optionsBox[i].addEventListener("click", function(){
-//     //         console.log(questions[currentQuestion].options[i])
-//     //         if (questions[currentQuestion].options[i] == questions[currentQuestion].answer) {
-//     //             correctAnswerCount++;
-//     //         }
-//     //     });
-
-//     // }   
-//     optionsBox.forEach((option, index)=>{
-//         option.textContent = questions[currentQuestion].options[index];
-//         option.classList.add("color-li");
-        
-//         option.addEventListener("click", function(){
-            
-//             console.log("Option clicked:" , questions[currentQuestion].question)
-//             // if (questions[currentQuestion].options[index] == questions[currentQuestion].answer) {
-//             //     correctAnswerCount++;
-//             // }
-
-//         });
-
-//     });
-//         addTimer()
-
-
-
-// }
-// function endQuiz(){
-//     alert(`quiz ended` + correctAnswerCount)
-
-// }
-
-import questions from './questions.js'
+import questionsArray from './questions.js'
 
 const start = document.getElementById('start-section');
 const timerSpace = document.getElementById("timer");
 const questionSpace = document.querySelector('h2');
 const optionsSpace = document.querySelectorAll('li');
 
-let currectQuestion = 0;
+let currentQuestion = 0;
 let correctAnswerCount = 0;
+let canAnswer = true;
+// a function to shuffle the questions in random ordered array
+function shuffleArray(array){
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; 
+    }
+    return array;
+}
+//initializing the questions and returning the shuffled array from the function
+const questions = shuffleArray(questionsArray)
 
+// this triggers the starting page code
 start.addEventListener('click', ()=>{
     start.style.display = "none";
     timerSpace.style.display="block";
     addTimer()
+    changeQuestion();
 })
 
-function addTimer(){
-    let time = 5;
-    const timer = setInterval(()=>{
-        timerSpace.innerHTML = time--;
-        if(currectQuestion.length ==questions.length){
-            return resultSection()
-        }
-        if(time < 0){
-            clearInterval(timer)
-            currectQuestion++;
-        }
-        else{
-            // changeQuestion()
-        }
 
+function addTimer() {
+    let time = 5;
+    const timer = setInterval(() => {
+        timerSpace.innerHTML = time--;
+        
+        if (time < 0) {
+            clearInterval(timer)
+            currentQuestion++;
+            if (currentQuestion >= questions.length) {
+                resultSection()
+                return
+            }
+            canAnswer=true
+            changeQuestion()
+            addTimer()
+        }
     }, 1000)
 
 }
 
 function changeQuestion(){
-    questionSpace.innerHTML = questions[currectQuestion].question;
+    questionSpace.innerHTML = questions[currentQuestion].question;
     optionsSpace.forEach(( option, index) => {
-        option.textContent = questions[currectQuestion].options[index];
+        option.textContent = questions[currentQuestion].options[index];
+        option.classList.remove("disabled", "li-selected");
         option.classList.add("color-li")
+        option.onclick = () => {
+            if (canAnswer) {
+                checkAnswer(option);
+            }
+        };
     })
-    optionsSpace.forEach((option)=>{
-        option.addEventListener('click', ()=>{
-            option.classList.add('li-selected')
-            if(option.textContent == questions[currectQuestion].answer)
-            console.log(questions[currectQuestion].answer)
-        })
-    })
-
 }
 
+function checkAnswer(selectedOption){
+    console.log(selectedOption.textContent);
+    selectedOption.classList.add("li-selected")
+    if(selectedOption.textContent === questions[currentQuestion].answer){
+        correctAnswerCount++;
+    }
+    canAnswer = false; 
+    disableOptions();
+} 
+
+function disableOptions(){
+    optionsSpace.forEach((option) => { option.classList.add("disabled");  })
+}
 function resultSection(){
-    const result = document.getElementById("")
+    timerSpace.style.display="none";
+    optionsSpace.forEach((option)=>{option.style.display="none";})
+    questionSpace.innerHTML = "Your Score : " + correctAnswerCount + "/" + questions.length;
 }
